3ee67b2657a74e23d33d7e8349a524c5d71a514f
sakita1210 <r.sakita@kura-corpo.co.jp>
2025-05-16 11:56:30 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -41,4 +41,19 @@ function App() {
   );
 }
 
+function My Button(){
+  return (
+    <button>I'm a button</button>
+  );
+} 
+export default function MyApp() {
+  return (
+    <div>
+      <h1>Welcome to my app</h1>
+      <MyButton />
+    </div>
+  );
+}
+
+
 export default App;
